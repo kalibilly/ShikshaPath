11969d2111df5936f2d531ae3c0211b9c06016db
kalibilly <arund1483@gmail.com>
2026-01-31 18:59:46 +0000

    Update settings.py

--- shikshapath/shikshapath/settings.py
+++ shikshapath/shikshapath/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xy=1(ijus9f*yq&@$^$%3im-08=^p+=f+x9h&8m-la%ohc!^r$'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -263,4 +263,5 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 # Firebase will be initialized in accounts/apps.py
+
 
