4aade816761f922aa27ea3e744ef0cc15a28a0bf
kalibilly <arund1483@gmail.com>
2026-02-15 21:33:48 +0000

    Update WSGI config to use deployment settings

--- shikshapath/wsgi.py
+++ shikshapath/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shikshapath.settings')
+settings_module = 'shikshapath.deployment_settings' if os.environ.get('RENDER_EXTERNAL_HOSTNAME') else os.environ.get('DJANGO_SETTINGS_MODULE', 'shikshapath.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+
 
 application = get_wsgi_application()
