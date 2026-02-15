37dd0c3964fd2d359032f911fe30909f07d31070
kalibilly <arund1483@gmail.com>
2026-02-15 21:31:10 +0000

    Refactor ASGI config to support deployment settings
    Update ASGI settings to use deployment settings based on environment.

--- shikshapath/asgi.py
+++ shikshapath/asgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shikshapath.settings')
+settings_module = 'shikshapath.deployment_settings' if os.environ.get('RENDER_EXTERNAL_HOSTNAME') else os.environ.get('DJANGO_SETTINGS_MODULE', 'shikshapath.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_asgi_application()
