bc2563d961fad0fc037fc5bbd96d108b213c80aa
kalibilly <arund1483@gmail.com>
2026-02-15 21:29:49 +0000

    Modify settings.py for production readiness
    Updated Django settings for production environment, including disabling debug mode and ensuring proper logging.

--- shikshapath/settings.py
+++ shikshapath/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '.onrender.com').split(',')
 
@@ -263,6 +263,3 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 # Firebase will be initialized in accounts/apps.py
-
-
-
