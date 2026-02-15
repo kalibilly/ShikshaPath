88e405401f02c5ba3c545c48704acdbd95c97191
kalibilly <arund1483@gmail.com>
2026-02-15 21:39:43 +0000

    Add files via upload

---
+++ shikshapath/deployment_settings.py
@@ -0,0 +1,38 @@
+import os
+from .settings import *
+from .settings import BASE_DIR
+import dj_database_url
+
+ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME')]  # Allow all hosts for cloud deployment (adjust as needed)
+CSRF_TRUSTED_ORIGINS = [f"https://{os.environ.get('RENDER_EXTERNAL_HOSTNAME')}"]  # Trust the cloud deployment domain for CSRF
+
+DEBUG = False # disable debug mode for production
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+STORAGES = {
+    'default': {
+        "BACKEND": 'storages.backends.s3boto3.S3Boto3Storage',
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+    )
+}
+
